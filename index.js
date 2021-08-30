5aa4699c514a70bdacb65bbe3b3de3673a5c2a5b
CactusKing101 <chalkking17@gmail.com>
2021-08-30 15:51:55 -0400

    sample.json

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
 const brain = require('brain.js');
 const { Client, Intents } = require('discord.js');
 const { main } = require('./general/token.json');
-const intents = new Intents(32767);
-const client = new Client({ intents: intents });
+const { SPT } = require('./general/sample.json');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 // create configuration for training
 const config = {
   iterations: 20000,
   log: true,
-  logPeriod: 50,
+  logPeriod: 100,
   layers: [10],
   errorThresh: 0.005,
 };
 
-// Replace this with a database collection that is structured like this retrain ai
-const data = [
-  { input: 'i want to end my self', output: 'Suicidal' },
-  { input: 'i want to hang my self', output: 'Suicidal' },
-  { input: 'i want to play a game', output: 'Normal' },
-  { input: 'do you want to play a game', output: 'Normal' },
-];
-
 const network = new brain.recurrent.LSTM();
-network.train(data, config);
+network.train(SPT, config);
 
 client.on('messageCreate', (msg) => {
   if (msg.author.bot) return;
